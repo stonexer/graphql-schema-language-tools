eff8c09adbbade6b3002afab0609c3bd73af64e7
SToneX <shitianxin@gmail.com>
2021-10-14 00:07:16 +0800

    feat: first type completion

--- packages/graphql-schema-monaco-editor/src/graphqlSchemaWorker.ts
+++ packages/graphql-schema-monaco-editor/src/graphqlSchemaWorker.ts
@@ -1,4 +1,6 @@
-import type { worker } from 'monaco-editor';
+import type { Position, worker } from 'monaco-editor';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+import { getGraphQLPositionByOffset } from 'graphql-schema-language-service';
 
 export interface ICreateData {
   languageId: string;
@@ -13,9 +15,44 @@ export class GraphQLSchemaWorker {
     this._languageId = 'graphql-schema';
   }
 
-  async doComplete() {
-    console.log('do complete', this._ctx, this._languageId);
+  async doComplete(uri: string, position: Position) {
+    const document = this._getTextDocument(uri);
+    const offset = document.offsetAt({
+      line: position.lineNumber,
+      character: position.column
+    });
+    const value = document.getText();
+    const visitorResult = getGraphQLPositionByOffset(value, offset);
+
+    if (visitorResult.position === 'NamedType') {
+      const completion = [
+        ...visitorResult.schema.objectTypes.map((typeItem) => ({
+          label: typeItem.value,
+          kind: 2,
+          insertText: typeItem.value,
+          detail: typeItem.description,
+          documentation: typeItem.Description
+        }))
+      ];
+
+      return completion as any;
+    }
 
     return [];
+  }
+
+  private _getTextDocument(uri: string): TextDocument {
+    let models = this._ctx.getMirrorModels();
+    for (let model of models) {
+      if (model.uri.toString() === uri) {
+        return TextDocument.create(
+          uri,
+          this._languageId,
+          model.version,
+          model.getValue()
+        );
+      }
+    }
+    throw new Error('no model');
   }
 }
--- packages/graphql-schema-monaco-editor/src/languageFeatures.ts
+++ packages/graphql-schema-monaco-editor/src/languageFeatures.ts
@@ -9,10 +9,13 @@ export class CompletionAdapter
     this._worker = _worker;
   }
 
-  async provideCompletionItems(model: monaco.editor.IReadOnlyModel) {
-    const worker = await this._worker(model.uri);
-
-    const suggestions = await worker.doComplete();
+  async provideCompletionItems(
+    model: monaco.editor.IReadOnlyModel,
+    position: monaco.Position
+  ) {
+    const resource = model.uri;
+    const worker = await this._worker(resource);
+    const suggestions = await worker.doComplete(resource.toString(), position);
 
     return Promise.resolve({ suggestions });
   }
